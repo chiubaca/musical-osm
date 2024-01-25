1984de13be19395e739545dde706c419ff913762
Alex Chiu <a.chiu@vu.city>
2024-01-25 15:38:26 +0000

    WIP: testing netlify functions to fix cors error

---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,19 @@
+export default async () => {
+
+  const MIRROR = 'https://planet.openstreetmap.org';
+
+  const response = await fetch(`${MIRROR}/replication/changesets/state.yaml`);
+
+  if (!response.ok) {
+    // TODO: Error handling
+    return;
+  }
+
+  
+  const text = await response.text();
+  console.log("🚀 ~ text:", text)
+  
+  
+  return new Response("Hello world")};
+
+export const config = { path: "/test" };
